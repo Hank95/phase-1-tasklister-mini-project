aa034a36a6fdb9460905a09412a5a851d7efde91
github username <github email address>
2021-06-15 16:52:19 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const tasks = document.querySelector("#tasks");
+
+  let buttons;
+
+  document.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let textValue = e.target.newtaskdescription.value;
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = "X";
+    button.className = "button";
+    li.innerText = textValue + "  ";
+    li.appendChild(button);
+    tasks.appendChild(li);
+
+    buttons = document.querySelectorAll(".button");
+    console.log(buttons);
+    for (let button of buttons) {
+      button.addEventListener("click", function (e) {
+        e.path[1].remove();
+      });
+    }
+  });
 });
